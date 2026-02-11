ea2c657e782156a11257ae97a3d6e4fd3d1767db
Sidhant Singh <singh.sidhant2004@gmail.com>
2026-02-11 20:40:35 +0530

    Fixed image loading and added validation

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
+#input image path here
 def extract_text(image_path):
     img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # noise removal
+    if img is None:
+        print("Image not found. Check file name/path.")
+        exit()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 3)
 
     text = pytesseract.image_to_string(gray)
